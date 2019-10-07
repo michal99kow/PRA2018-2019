b49271fbe525b164c17ea07700fada7e5157bb93
s444410 <michal99kow@gmail.com>
2019-10-07 12:57:04 +0200

    moj pierwszy commit

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+
+        Logger log = Logger.getLogger("name");
+        log.info("message");
     }
 }
